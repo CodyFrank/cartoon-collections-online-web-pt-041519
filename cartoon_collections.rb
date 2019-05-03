1bf473c94dce11779772c25ead2d5f7d1357dd9c
Cody Frank <cody.frank30@gmail.com>
2019-05-03 00:38:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,10 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |word|
+    word.capitalize << !
+  end
 end
 
 def long_planeteer_calls# code an argument here
